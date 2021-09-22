b4ad0746dce2bde30f8e179dc7559fa596439ea3
DucTran2310 <18520021@gm.uit.edu.vn>
2021-09-22 19:27:07 +0700

    Update every things

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import Jumbotron from "components/jumbotron";
+import jumboData from "./fixtures/jumbo";
 import React from "react";
 
 export default function App() {
     return (
-        <p>Hello</p>
+        <Jumbotron.Container>
+            {jumboData.map((item) => (
+                <Jumbotron
+                    key={item.id}
+                    direction= {item.direction}
+                >
+                    <Jumbotron.Pane>
+                        <Jumbotron.Title>{item.title}</Jumbotron.Title>
+                        <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+                    </Jumbotron.Pane>
+                    <Jumbotron.Pane>
+                        <Jumbotron.Image src={item.image} alt={item.alt} />
+                    </Jumbotron.Pane>
+                </Jumbotron>
+            ))}
+        </Jumbotron.Container>
     );
 }
 
