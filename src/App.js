59c86512005f2e84a76063bc54bb2ea809911662
DucTran2310 <18520021@gm.uit.edu.vn>
2021-09-23 21:56:33 +0700

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { FaqsContainer } from "containers/faqs";
+import { FooterContainer } from "containers/footer";
 import { JumbotronContainer } from "containers/jumbotron";
 import React from "react";
 
 export default function App() {
-    return <JumbotronContainer />;
+    return (
+        <>
+            <JumbotronContainer />
+            <FaqsContainer />
+            <FooterContainer />
+        </>
+    );
 }
