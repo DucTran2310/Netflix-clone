da84da03432fc843272490c7848b055ca3c2ae36
DucTran2310 <18520021@gm.uit.edu.vn>
2021-09-27 20:18:10 +0700

    Update component

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -28,8 +28,10 @@ export const Title = styled.h1`
 
 export const Item = styled.div`
     color: white;
+    margin: auto;
     margin-bottom: 10px;
-    max-width: 670px;
+    max-width: 700px;
+    width: 100%;
 
     &:first-of-type {
         margin-top: 3rem;
