ac3171b37793f4762a6dd357d8bb2ff59f0db940
DucTran2310 <18520021@gm.uit.edu.vn>
2021-09-27 20:24:47 +0700

    Update everythings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import { FaqsContainer } from "containers/faqs";
-import { FooterContainer } from "containers/footer";
-import { JumbotronContainer } from "containers/jumbotron";
 import React from "react";
+import { Home, Browse, Signin, Signup } from "./pages";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
+import { PushDataToFireStore } from "lib/firebase.prod";
 
 export default function App() {
+    PushDataToFireStore();
+
     return (
-        <>
-            <JumbotronContainer />
-            <FaqsContainer />
-            <FooterContainer />
-        </>
+        <Router>
+            <Route exact path={ROUTES.BROWSE}>
+                <Browse />
+            </Route>
+            <Route exact path={ROUTES.SIGN_IN}>
+                <Signin />
+            </Route>
+            <Route exact path={ROUTES.SIGN_UP}>
+                <Signup />
+            </Route>
+            <Route exact path={ROUTES.HOME}>
+                <Home />
+            </Route>
+        </Router>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import React from "react";
 import "normalize.css";
 import ReactDOM from "react-dom";
 import App from "./app";
+import { firebase } from "./lib/firebase.prod";
+import { FirebaseContext } from "context/firebase";
 
 ReactDOM.render(
-    <>
-        <GlobalStyles />
-        <App />,
-    </>,
+    <React.StrictMode>
+        <FirebaseContext.Provider value={{ firebase }}>
+            <GlobalStyles />
+            <App />
+        </FirebaseContext.Provider>
+    </React.StrictMode>,
     document.getElementById("root")
 );
